7653555aa51cfe8026e3c8945fcb850b9a43860a
arshiyasawhney <72881909+arshiyasawhney@users.noreply.github.com>
2021-02-13 13:27:18 +0000

    Add files via upload

---
+++ c-37/sketch.js
@@ -0,0 +1,26 @@
+var hypnoticBall, database;
+var position;
+var gameState = 0, playerCount = 0;
+var game, form, player;
+var allPlayers;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  background("white");
+  if (playerCount === 4){
+    game.update(1);
+  }
+  if (gameState === 1){
+    clear();
+    game.play();
+  }
+}
+
